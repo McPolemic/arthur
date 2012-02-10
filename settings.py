827d3f28306781f1a36798956c5d35fed5df93b2
Adam Lukens <adam.lukens@hp.com>
2012-02-10 11:50:09 -0500

    Add placeholder settings to settings.py

--- settings.py
+++ settings.py
@@ -8,3 +8,22 @@ global_conf = {
 #Set revision date to today
 for i in (program_specs, unit_test, implementation):
 	i['revision_history'][0]['last_update'] = date.today().strftime('%m/%d/%Y')
+
+#The pairs are "file to be generated" and "template file"
+#files = {
+#    'Implementation Plan': {
+#        'template_name': 'Implementation Plan.htm',
+#        'settings_dict': implementation,
+#        'output': './output/%s - Implementation Plan.html' % global_conf['number']
+#    },
+#    'Program Specifications': {
+#        'template_name': 'Program Specifications.htm',
+#        'settings_dict': program_specs,
+#        'output': './output/%s - Program Specifications.html' % global_conf['number']
+#    },
+#    'Unit Testing': {
+#        'template_name': 'Unit Testing.htm',
+#        'settings_dict': unit_test,
+#        'output': './output/%s - Unit Testing.html' % global_conf['number']
+#    }
+#}
