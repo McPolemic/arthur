0bc144f80a88e08c3689e2076ef8772493c4388a
Adam Lukens <adam.lukens@hp.com>
2012-02-10 14:44:38 -0500

    Added a dict-like object to enable optional keys in the dictionaries for settings

--- settings_defaults.py
+++ settings_defaults.py
@@ -1,6 +1,43 @@
 #!/usr/bin/python
 from markdown import markdown as md
 
+class Opt(dict):
+    """A dictionary that returns a blank string if an attribute isn't found"""
+    def __init__(self, dict=None, **kwargs):
+        self.data = {}
+        if dict is not None:
+            self.update(dict)
+        if len(kwargs):
+            self.update(kwargs)
+    
+    def update(self, dict=None, **kwargs):
+        if dict is None:
+            pass
+        elif isinstance(dict, Opt):
+            self.data.update(dict.data)
+        elif isinstance(dict, type({})) or not hasattr(dict, 'items'):
+            self.data.update(dict)
+        else:
+            for k, v in dict.items():
+                self[k] = v
+        if len(kwargs):
+            self.data.update(kwargs)
+
+    def __repr__(self): return repr(self.data)
+    def __len__(self): return len(self.data)
+    def __getitem__(self, key): return self.data[key] if key in self.data else ''
+    def __setitem__(self, key, item): self.data[key] = item
+    def __delitem__(self, key): del self.data[key]
+    def __contains__(self, key): return key in self.data
+    def __getattr__(self, key): return self.__getitem__(key)
+    def clear(self): self.data.clear()
+    def keys(self): return self.data.keys()
+    def items(self): return self.data.items()
+    def values(self): return self.data.values()
+    def iteritems(self): return self.data.iteritems()
+    def iterkeys(self): return self.data.iterkeys()
+    def itervalues(self): return self.data.itervalues()
+
 global_conf = {
 	'number': '15135',
 }
@@ -20,7 +57,16 @@ program_specs = {
     "program_specs_intro": """Program Specifications description goes here.""",
     
     "modules": [
-        {
+        Opt({
+            "name": "clmsub92.sc",
+            "purpose": "Main UB92 claims function",
+            "prerequisites": "N\A",
+            "function": "ub92Main()",
+            "description": md("""This is a **multi-line**
+
+string""")
+        }),
+        Opt({
             "name": "clmsub92.sc",
             "purpose": "Main UB92 claims function",
             "prerequisites": "N\A",
@@ -58,7 +104,7 @@ program_specs = {
     {
        xref_dtlApplyTpl();
     }</pre>"""
-        }
+        }),
     ]
 }
 
