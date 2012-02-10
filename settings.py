4503ef62b4ad8b429356a59306f8f31d2bf0dad8
Adam Lukens <adam.lukens@hp.com>
2012-02-10 11:16:04 -0500

    Removed stray text from settings

--- settings.py
+++ settings.py
@@ -8,9 +8,3 @@ global_conf = {
 #Set revision date to today
 for i in (program_specs, unit_test, implementation):
 	i['revision_history'][0]['last_update'] = date.today().strftime('%m/%d/%Y')
-
-[{
-        "last_update": "10/28/2011",
-        "author": "Adam Lukens",
-        "description": "Initial Creation"
-    }],
